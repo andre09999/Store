1f095008fec34628631c058968f5994eb816b033
andre09999 <andre09999@live.com>
2022-09-03 13:51:21 -0300

    Updates

---
+++ precache-manifest.a0ca837e2240936a6029a8d5a870fd8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913122c04e0f6bef1477ebf5a3e6db03",
+    "url": "/Online-store/index.html"
+  },
+  {
+    "revision": "f330a276f59bd9a86846",
+    "url": "/Online-store/static/css/main.4f006034.chunk.css"
+  },
+  {
+    "revision": "bb98146285f301232ec4",
+    "url": "/Online-store/static/js/2.9b998732.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Online-store/static/js/2.9b998732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f330a276f59bd9a86846",
+    "url": "/Online-store/static/js/main.4bdae4c5.chunk.js"
+  },
+  {
+    "revision": "46e3c6a622c010eec13e",
+    "url": "/Online-store/static/js/runtime-main.58a63d36.js"
+  }
+]);
